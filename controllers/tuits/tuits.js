59620725e56738e62ca3dc8eb137b37831b7e328
chenky5354 <chen.ky@northeastern.edu>
2022-11-20 20:22:41 -0500

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        "_id": 123, "topic": "Space", "username": "SpaceX",
+        "handle": "@spacex", "time": "2h", "image": "https://imgs.search.brave.com/JKDkrEPDJH8sZpfKg6PREpotMjurAwQqyhRB5GGWWEo/rs:fit:200:200:1/g:ce/aHR0cHM6Ly9tZWRp/YS1leHAxLmxpY2Ru/LmNvbS9kbXMvaW1h/Z2UvQzU2MEJBUUVi/cUxRLUpFMHZkUS9j/b21wYW55LWxvZ29f/MjAwXzIwMC8wP2U9/MjE1OTAyNDQwMCZ2/PWJldGEmdD1LbXRE/VW5nTGpLVklxanNq/WjljM0lFTnd1blFB/R2thdTNfQVpoOXJT/ZU9n",
+        "title": "SpaceX's Mission",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "liked": true, "likes": 2345,
+        "disliked": false, "dislikes": -20,
+        "replies": 123, "retuits": 432
+    },
+    {
+        "_id": 234, "topic": "Traffic", "username": "The Boring Company",
+        "handle": "@boringcompany", "time": "2h", "image": "https://imgs.search.brave.com/58d6lr8QXLw4tKIRUivz6f0F4lFsYqyvcfCFBtURvs0/rs:fit:800:800:1/g:ce/aHR0cHM6Ly9pbWFn/ZS5hcnRpc3RzaG90/LmNvbS9kLjY4ODMy/NzIuMTcxODkwLnMz/LjEtMjkyOTI5LVlY/SjBhWE4wYzJodmRB/LTgwMHg4MDAuanBn",
+        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+        "liked": true, "likes": 2345,
+        "disliked": true, "dislikes": -11,
+        "replies": 123, "retuits": 432
+    },
+];
